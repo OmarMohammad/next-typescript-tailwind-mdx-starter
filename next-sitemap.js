68ee468e6ac26c2f7084e8313fa9d62960f16666
OmarMohammad <omar.ibn.mohammad@gmail.com>
2020-12-31 16:02:56 +0100

    Change seo

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,7 @@
 require('dotenv-flow').config()
 
 module.exports = {
-  siteUrl: `https://example.com`,
+  siteUrl: `https://omarmohammad.net`,
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 5000,
